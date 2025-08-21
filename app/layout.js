1371ce8f5ecbaacfe606b1a30b1ceb8424d8fae6
bagdenius <bagdensparrow@gmail.com>
2025-08-21 22:16:16 +0300

    setup tailwind css and globals with color theme system

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
 
+import '@/app/_styles/globals.css';
+
 export const metadata = { title: 'The Wild Oasis' };
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>
+      <body className='bg-primary-950 text-primary-100 min-h-dvh'>
         <header>
           <Logo />
           <Navigation />
